0e5ae2e3d0f223bf7f436cffe57786707662732e
boimlohdi <boim.lohdi@gmail.com>
2026-06-14 13:52:08 +0700

    Update MainActivity.java

--- android-src/MainActivity.java
+++ android-src/MainActivity.java
@@ -9,9 +9,23 @@ public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Bypass ngrok warning page
         WebView webView = getBridge().getWebView();
         WebSettings settings = webView.getSettings();
         String defaultUA = settings.getUserAgentString();
         settings.setUserAgentString(defaultUA + " ngrok-skip-browser-warning");
+
+        // Enable JavaScript features needed for QR scanning
+        settings.setJavaScriptEnabled(true);
+        settings.setMediaPlaybackRequiresUserGesture(false);
+        settings.setAllowFileAccess(true);
+        settings.setDomStorageEnabled(true);
+        settings.setDatabaseEnabled(true);
+
+        // Enable camera access in WebView
+        settings.setAllowContentAccess(true);
+        settings.setAllowFileAccessFromFileURLs(true);
+        settings.setAllowUniversalAccessFromFileURLs(true);
     }
 }
